0ef44245f3b0c3add4fd4986d980a97fc280f152
mechev36 <mate.echeverri@gmail.com>
2018-04-06 06:41:54 -0500

    Create the theoric model

--- LinearModels.R
+++ LinearModels.R
@@ -6,7 +6,11 @@ x4 = sqrt(x2) #sqrt(Height)
 x5 = x1*x1 #Girth^2
 x6 = x2*x2 #Height^2
 
-myData<- list("y"=y,"x1"=x1,"x2"=x2,"x3"=x3,"x4"=x4,"x5"=x5,"x6"=x6)
+myData<- list("y"=y,"x1"=x1,"x2"=x2,"x3"=x3,"x4"=x4,"x5"=x5,"x6"=x6, "r"=(x1/2*pi))
+
+pef<- lm(y~(r^2*x2), data = myData)
+summary(pef)
+
 
 model1 <- lm(y~x1+x2+x3+x4+x5+x6, data = myData)
 
